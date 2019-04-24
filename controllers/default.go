a2f2d6d7d4282552cf9980ea8e17d84942f4b9d6
lijinghuatongxue <woshilijinghua@gmail.com>
2019-04-24 17:48:29 +0800

    orm delete

--- controllers/default.go
+++ controllers/default.go
@@ -15,10 +15,10 @@ type MainController struct {
 /* 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"  */
 
-// orm insert 操作
+// +++++++++++++++++++++++++++++++++++++++++++++++++  orm insert 操作
 
-/*	//有orm对象
-	o := orm.NewOrm()
+	//有orm对象
+/*	o := orm.NewOrm()
 	// 有一个要插入数据的结构体对象
 	user := models.User{}
 	// 对结构体对象赋值
@@ -32,7 +32,7 @@ type MainController struct {
 		return
 		}*/
 
-// orm select 操作
+// +++++++++++++++++++++++++++++++++++++++++++++++++  orm select 操作
 
 // 1.  有orm对象
 /*	o := orm.NewOrm()
@@ -50,9 +50,9 @@ type MainController struct {
 	 }
 	logs.Info( "查询成功",user)*/
 
-// orm update 操作
+// +++++++++++++++++++++++++++++++++++++++++++++++++   orm update 操作
 // 1 要有orm对象
-	o := orm.NewOrm()
+/*	o := orm.NewOrm()
 // 2 需要更新的结构体对象
 	user := models.User{}
 // 3 查到需要更新的数据
@@ -62,7 +62,8 @@ type MainController struct {
 
 // 4 给数据重新赋值
 	if err == nil {
-		user.Name = "111"
+		user.Name = "121"
+		// 如果只更新name的话，就把下面的注释掉，程序依旧可行
 		user.Pwd = "333"
 	}
 // 5 更新
@@ -70,7 +71,25 @@ type MainController struct {
 	if err != nil{
 		logs.Info( "更新失败",err)
 		return
+	}*/
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++   orm delete 操作
+
+// 有orm对象
+	o := orm.NewOrm()
+// 删除的对象
+	user := models.User{}
+// 指定删除哪一条数据
+	user.Id = 1
+// 删除
+	_,err := o.Delete(&user)
+	if err != nil{
+		logs.Info("删除失败",err)
+		return
 	}
+//
+
+
 
 	c.Data["data"] = "home页"
 	c.TplName = "test.html"
