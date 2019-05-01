7e37cfd104c65f285ab2c74c1e80bcdeb39782e7
lijinghuatongxue <woshilijinghua@gmail.com>
2019-05-01 22:52:59 +0800

    添加文章标题前

--- models/models.go
+++ models/models.go
@@ -9,16 +9,30 @@ import (
 //表的设计
 type User struct{
 	Id int
-	Name string
+	// 后面添加unique等设置的话，必须删除掉表重新建，设置才会生效
+	Name string `"orm:unique"`
 	Pwd string
 }
 
 // 文章结构体，不建议字段中间使用大写字母或者下划线
 type Article struct {
-	Id int
-	Aname string
+	// `pk` 主键
+	// `auto` 自增
+	//数据库字段的属性设置
+	//设置主键pk
+	//自增auto
+	//全局唯一 unique
+	//设置允许为空 null
+	//设置长度  size()
+	//设置默认值 default()
+	//浮点数精度  digits(12);decimal(4)      12345678.1234
+	//时间的设置  date   datetime   time.time    type(date)/type(datetime)
+	//每次model更新时，都会对时间自动更新	auto_now
+	//只有第一次保存时，才更新时间			    auto_now_add
+	Id int `orm:"pk;auto"`
+	ArtiName string `orm:"size(30)"`
 	Atime time.Time
-	Acount int
+	Acount  int `orm:"default(0);null"`
 	Acontent string
 	Aimg string
 }
